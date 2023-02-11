e404a4a3adc789f82e165245e591dce972de47f0
jiritichycode <jiri.tichy@hisim.cz>
2023-02-11 15:43:49 +0100

    + storybook addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 module.exports = {
   "stories": ["../stories/**/*.stories.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "storybook/addon-storysource"],
   "framework": "@storybook/html",
   core: {
     builder: "webpack5"
