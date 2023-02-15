c5d1d8f0707a0126bda68f7dab436fc5ec161da7
jiritichycode <jiri.tichy@hisim.cz>
2023-02-15 16:59:34 +0100

    + js for contact form

--- scripts/app-main.js
+++ scripts/app-main.js
@@ -1,6 +1,8 @@
 import {menuToggle} from './components/menu';
 import {modal} from './components/modal';
+import {contactForm} from './components/contact-form';
 
 menuToggle();
 modal('js-open-modal');
+contactForm('contact-form', 'contact-form-submit');
 
